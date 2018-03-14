cbc048db3bde7ec3a0f8f7c8088781e596086501
Samuel Fox <samf1596@gmail.com>
2018-03-14 11:06:45 -0400

    removed my secret

--- crypto_web/settings.py
+++ crypto_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-q@gdw63aesdmn=1&a5#+rp3&ud4h38k^nff&^)kmm=ss#w$!s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
