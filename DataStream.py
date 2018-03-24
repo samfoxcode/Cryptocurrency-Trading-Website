373371fa766b8b4b26d9af3a490fe73e40f3df3b
marcgehman <marcgehman@gmail.com>
2018-03-24 01:06:33 -0400

    DataStream should now work with manage.py

--- DataStream.py
+++ DataStream.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from django.utils import timezone
 import time
 import threading
 import json
@@ -7,18 +8,15 @@ import sys
 
 from home.models import Old_Prices, Coins
 
-def StoreData():
-    pass
-
 def DataStream():
     while(True):
         #set the time to stream the data
-        timeToRun = datetime.now() + timedelta(seconds=10)
+        timeToRun = datetime.now() + timedelta(seconds=30)
 
         #Request data from coinmarketcap and parse the data
         r = requests.get('https://api.coinmarketcap.com/v1/ticker/')
+        print("--------------------------------------------------------------------------------------------------------------------------")
         for coin in r.json():
-
             print(datetime.now(), coin["symbol"], coin["price_usd"],)
             if not Coins.objects.filter(ticker = coin["symbol"]).exists():
                 coinz = Coins(ticker = coin["symbol"], website = "http", current_price = coin["price_usd"], gain_loss = 0)
@@ -26,7 +24,7 @@ def DataStream():
             else:
                 coinz = Coins.objects.get(ticker= coin["symbol"])
 
-            Price = Old_Prices(timestamp = datetime.now(), ticker = coinz, accuracy_projection = 0, price = coin["price_usd"])
+            Price = Old_Prices(timestamp = timezone.now(), ticker = coinz, accuracy_projection = 0, price = coin["price_usd"])
             Price.save()
 
         # Wait until timeToRun has passed to fetch new data
@@ -38,5 +36,3 @@ t1 = threading.Thread(target=DataStream)
 def StartStream():
     # Create a thread to run the data stream
     t1.start()
-
-StartStream()
