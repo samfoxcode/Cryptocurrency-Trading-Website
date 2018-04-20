cd9c633647e732e297542ee0c12a7c66ebb23966
Rikesh Sharma <Rikesh@Rikeshs-MacBook-Pro.local>
2018-04-19 23:18:49 -0400

    sign in works now, created user.html

--- account/views.py
+++ account/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate
 def account(request):
-    return render(request, 'account/accountmain.html')
+    if not request.user.is_authenticated:
+        return render(request,'account/accountmain.html')
+    context = {
+        "user": request.user
+    }
+    return render(request,'account/user.html',context)
