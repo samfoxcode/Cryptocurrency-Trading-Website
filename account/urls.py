a5be68146f2f87b7520cc48a81e865b0ce0473bc
Samuel Fox <samf1596@gmail.com>
2018-04-23 11:15:24 -0400

    fixed changes that I overwrote from Ricky

--- account/urls.py
+++ account/urls.py
@@ -8,15 +8,8 @@ urlpatterns = [
     path('', views.search, name='search'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-<<<<<<< HEAD
-    path('logout/',views.logout_view,name= 'logout'),
-    path('sell/', views.sell, name='sell'),
-    path('buy/', views.buy, name='buy')
-
-=======
     path('logout/',views.logout_view,name = 'logout'),
     path('buy/', views.buy, name='buy'),
     path('sell/', views.sell, name='sell'),
     path('load_info/', views.load_info, name='load_info')
->>>>>>> sams_workingbranch
 ]
